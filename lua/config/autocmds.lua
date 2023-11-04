93e806c808d0107bfe564c06544e6b3a24cb2871
Felix Zippel <contact@zippelf.com>
2023-11-04 18:10:27 +0100

    Update autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,11 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local Fugitive_Custom = vim.api.nvim_create_augroup("Fugitive_Custom", {})
+local Custom_Fugitive = vim.api.nvim_create_augroup("Custom_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-  group = Fugitive_Custom,
+  group = Custom_Fugitive,
   pattern = "*",
   callback = function()
     if vim.bo.ft ~= "fugitive" then
@@ -14,18 +14,22 @@ autocmd("BufWinEnter", {
     end
 
     local bufnr = vim.api.nvim_get_current_buf()
-    local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "<leader>p", function()
+    vim.keymap.set("n", "<leader>gp", function()
       vim.cmd.Git("push")
-    end, opts)
+    end, { buffer = bufnr, remap = false, desc = "Perform [G]it [P]ush" })
 
     -- rebase always
-    vim.keymap.set("n", "<leader>P", function()
+    vim.keymap.set("n", "<leader>gP", function()
       vim.cmd.Git({ "pull", "--rebase" })
-    end, opts)
+    end, { buffer = bufnr, remap = false, desc = "Perform [G]it [P]ull with rebase" })
 
     -- NOTE: It allows me to easily set the branch i am pushing and any tracking
     -- needed if i did not set the branch up correctly
-    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+    vim.keymap.set(
+      "n",
+      "<leader>t",
+      ":Git push -u origin ",
+      { buffer = bufnr, remap = false, desc = "Se[t] upstream and push" }
+    )
   end,
 })
