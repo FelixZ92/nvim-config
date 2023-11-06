2c4723592822e64f4184f9d60731aaee6612ef68
Felix Zippel <contact@zippelf.com>
2023-11-06 02:30:25 +0100

    Finally working k8s intellisense :)

--- lua/plugins/yaml.lua
+++ lua/plugins/yaml.lua
@@ -4,18 +4,16 @@ return {
     "neovim/nvim-lspconfig",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
+    "diogo464/kubernetes.nvim",
   },
+  lazy = false,
   config = function()
     require("telescope").load_extension("yaml_schema")
     local cfg = require("yaml-companion").setup({
       schemas = {
         {
-          name = "Kubernetes 1.28.3",
-          uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.28.3-standalone-strict/all.json",
-        },
-        {
-          name = "ExternalSecret v1beta1",
-          uri = "/Users/felix/src/github.com/felixz92/crd-scraper/json-schema.json",
+          name = "Kubernetes full with CRDs",
+          uri = require("kubernetes").yamlls_schema(),
         },
       },
     })
